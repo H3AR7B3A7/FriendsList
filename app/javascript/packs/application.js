5d4ee87d2bd8f16dda1b48d69d69618e1e8eb251
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-29 05:14:22 +0100

    :children_crossing: Added Custom Dialog

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,25 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+import Rails from "@rails/ujs";
+
+// Overrides default rails confirmation with bootstrap 4 confirm dialog
+Rails.confirm = function(message, element) {
+  let $element = $(element)
+  let $dialog = $('#confirmation-modal')
+  let $content = $dialog.find('#modal-content')
+  let $ok = $dialog.find('#ok-button')
+  $content.text(message)
+  $ok.click(function(event) {
+      event.preventDefault()
+      $dialog.modal("hide")
+      let old = Rails.confirm
+      Rails.confirm = function() { return true }
+      $element.get(0).click()
+      Rails.confirm = old
+    })
+  $dialog.modal("show")
+  return false;
+}
